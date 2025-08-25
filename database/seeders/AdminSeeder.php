f38beaa3fc75b7799ebea444b4c497fa9668cd9f
rabbitmaid <rabbitmaid@proton.me>
2025-08-25 18:32:52 +0100

    Added the admin user seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         Admin::create([
+            'username' => 'adminuser',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('password'), // Always hash passwords
+            'course_id' => 1,
+            'status_id' => 1,
+            'gender_id' => 1,
+            'date_of_birth' => '1995-05-20',
+            'role_id' => 1 // e.g., 1 = Super Admin
+        ]);
+    }
+}
